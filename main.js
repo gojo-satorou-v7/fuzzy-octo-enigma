9c89070d08cec20881969e5293b6b48718fbace0
gojo-satorou-v7 <118048127+gojo-satorou-v7@users.noreply.github.com>
2024-12-27 00:12:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ self.addEventListener('install', function(event) {
     if(e.request.url=="https://gojo-satorou-v7.github.io/fuzzy-octo-enigma/data"){
         console.log("match");
       e.respondWith(fetch("/").then(()=>{
-          return Response.redirect("data://hackerone.com/html,<script>alert(3)</script>");
+          return Response.redirect("about:net-internals");
       }));
     }
   }
