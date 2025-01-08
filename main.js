f6b34bce99ba9a899fa3ac27b933c4ca58ca9b2e
gojo-satorou-v7 <118048127+gojo-satorou-v7@users.noreply.github.com>
2025-01-08 12:08:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ self.addEventListener('install', function(event) {
     if(e.request.url=="https://gojo-satorou-v7.github.io/fuzzy-octo-enigma/404.html"){
         console.log("match");
       e.respondWith(fetch("/").then(()=>{
-          return Response.redirect("https://example.com");
+          return Response.redirect("intent://www.google.com/hsi?name=Chrome&icon=https://ssl.gstatic.com/shortcuts/android/home/v1/192px/weather_g_badged.png&ve=145829&hv=1&dest=1&source=homescreen_shortcut&query=https://ndev.tk#Intent;scheme=https;package=com.google.android.googlequicksearchbox;end");
       }));
     }
   }
